550be48d74fd6d6bd7e4c8c07966dcb540f3e8d5
ashes999 <ashes999@yahoo.com>
2015-12-21 15:42:55 -0500

    Added Element class with x/y properties. For #1

---
+++ source/deengames/abook/core/Element.hx
@@ -0,0 +1,12 @@
+package deengames.abook.core;
+
+class Element {
+
+  public var x(default, null):Int;
+  public var y(default, null):Int;
+
+  public function new(x:Int, y:Int) {
+    this.x = x;
+    this.y = y;
+  }
+}
