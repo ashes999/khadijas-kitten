a4bfaa81828f2d4636cbbc18080c3a5574523af1
ashes999 <ashes999@yahoo.com>
2016-06-04 13:01:11 -0400

    Add media; integrate Flurry Analytics

--- source/Reg.hx
+++ source/Reg.hx
@@ -4,6 +4,6 @@ import flixel.util.FlxSave;
 
 class Reg
 {
-	public static var flurryKey = 'INSERT FLURRY KEY HERE';
+	public static var flurryKey = 'WPVK323GK8D865WJTY8M';
 	public static var googleAnalyticsUrl = '/stats/owlicious.html';
 }
